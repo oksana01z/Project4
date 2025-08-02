66fa5ebca3226160b13b2a559832e20ba7656b90
oksana01z <shalty@mail.ru>
2025-08-02 21:40:58 +0300

    первый коммит

--- main.py
+++ main.py
@@ -11,4 +11,43 @@ app = Client(
     api_id=config.API_ID,
     api_hash=config.API_HASH,
     bot_token=config.BOT_TOKEN)
+
+@app.on_message(filters.command("start"))
+async def start_command(client: Client, message: Message):
+    print("/start")
+    await message.reply(" Напиши /quiz чтобы начать")
+
+@app.on_message(filters.command("quiz"))
+async def quiz_command(client, message):
+    text, buttons, question = get_question()
+    buttons.append(InlineKeyboardButton("Следующий вопрос ", callback_data="next"))
+    await message.reply(
+        text,
+        reply_markup=InlineKeyboardMarkup([buttons]))
+
+
+@app.on_callback_query()
+async def handle_answer(client, callback_query):
+    data = callback_query.data
+
+    if data == "next":
+        text, buttons, question = get_question()
+        buttons.append(InlineKeyboardButton("Следующий вопрос ", callback_data="next"))
+        await callback_query.message.edit_text(
+            text,
+            reply_markup=InlineKeyboardMarkup([buttons])
+        )
+        return
+
+    selected_index = int(data)
+    text, buttons, question = get_question()
+    correct = question["answers"][selected_index]["correct"]
+
+    if correct:
+        result = "Правеьно"
+    else:
+        result = "Неправильно"
+
+    await callback_query.answer(result, show_alert=True)
+
 app.run()
